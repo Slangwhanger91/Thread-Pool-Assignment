d479e5cc7027a0184a241bd8d1ac5ce96d340237
amit <amitzviklerr@hotmail.com>
2015-04-05 17:10:12 +0300

    Merged PoolManager with PoolThread and further constructed baselines

--- Thread-Pool-Assignment/src/PoolManager.java
+++ Thread-Pool-Assignment/src/PoolManager.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 public class PoolManager {
 	private PoolThread[] threadsArr;
@@ -7,19 +9,36 @@ public class PoolManager {
 	private boolean NoTasks;
 	Object lock;
 	
+	private int s, m;
+	
 	private boolean stop_and_exit;
-
-	PoolManager(int p/*TODO*/){
+	
+	PoolManager(int p){
 		threadsArr = new PoolThread[p];
-		//PM_Tasks = new Task[t];
 
-		All_Threads_Busy = NoTasks = stop_and_exit = false;
+		NoTasks = true;
+		All_Threads_Busy = stop_and_exit = false;
 
 		for (PoolThread PT : threadsArr) PT = new PoolThread();
 
 		//TODO PM_Tasks
 
 		execute();
+	}
+	
+	/**
+	 * @param k - the number of expressions of the form (1.1) that have to be evaluated. <b>(Feeder related)</b>.
+	 * @param r - the number of expressions of the form (1.2) that have to be evaluated. <b>(Feeder related)</b>.
+	 * @param n_values_1 - the k values of n for each expression (1.1) that have to be evaluated. <b>(Feeder related)</b>.
+	 * @param l_values_2 - the r values of ` for each expression (1.2) that have to be evaluated. <b>(Feeder related)</b>.
+	 * @param m_values_2 - the r values of ` for each expression (1.2) that have to be evaluated. <b>(Feeder related)</b>.
+	 * @param t - is the limit to the amount of tasks stored within the <b>PoolManager</b>.
+	 * @param s - the number of summands each <b>PoolThread</b> is allowed to execute.
+	 * @param m - the number of multiplicands each <b>PoolThread</b> is allowed to execute.
+	 */
+	public void solution(int k, int r, int[] n_values_1, int[] l_values_2, int[] m_values_2,
+			int t, int s, int m){
+		//TODO
 	}
 
 	private void execute(){
@@ -38,5 +57,38 @@ public class PoolManager {
 	/**Stops the PoolManager from working and exit.*/
 	public void stop_and_exit(){
 		stop_and_exit = true;
+	}
+	
+	
+	
+	
+	/**Must only be known to PoolManager and have no interaction with any other class.*/
+	class PoolThread extends Thread{
+
+		private Task task;
+		private Object lock = new Object();
+
+		/**Gives this thread a task to perform TODO times*/
+		public void set_task(Task T) {
+			task = T;
+			lock.notify();
+		}
+
+		public void run(){
+			while(true){
+				//TODO perform task
+
+				synchronized(lock){
+					try {lock.wait();} catch (InterruptedException e) {e.printStackTrace();}
+				}
+			}
+		}
+
+		@SuppressWarnings("serial")
+		class PoolThreadException extends Exception{
+			public PoolThreadException (){
+				super("Method NULL Pointer Exception.");
+			}
+		}
 	}
 } 
--- Thread-Pool-Assignment/src/User.java
+++ Thread-Pool-Assignment/src/User.java
@@ -2,7 +2,36 @@
 public class User {
 
 	public static void main(String[] args) {
-
+		
+		PoolManager.PoolThread t1 = new PoolManager(10).new PoolThread();
+		t1.start();
+		try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+		System.out.println("get state: " + t1.getState());
+		if(t1.getState() == Thread.State.WAITING)
+			System.out.println("Waiting: true");
 	}
 
+
+
 }
+
+/*class PoolThread extends Thread{
+
+	Task task;
+	Object lock = new Object();
+
+	public void set_task(Task T){
+		task = T;	
+		lock.notify();
+	}
+
+	public void run(){
+		while(true){
+			//perform task
+			
+			synchronized(lock){
+				try {lock.wait();} catch (InterruptedException e) {e.printStackTrace();}
+			}
+		}
+	}
+}*/
