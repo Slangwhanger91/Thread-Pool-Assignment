a9f7c41c1403ee3e8713fd54429a46fa752bd948
amit <amitzviklerr@hotmail.com>
2015-04-05 17:43:08 +0300

    updated Task

--- Thread-Pool-Assignment/src/Task.java
+++ Thread-Pool-Assignment/src/Task.java
@@ -1,42 +1,43 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 abstract public class Task {
-	private boolean isDone;
-	private int result;
+	AtomicInteger final_result;
 	
 	public Task(){
-		isDone = false;
-		result = 0;
-	}
-	
-	public boolean isDone(){
-		return isDone;
+		final_result = new AtomicInteger(0);
 	}
 	
 	public void fill_result(int partial_result){
-		result += partial_result;
+		final_result.addAndGet(partial_result);
 	}
 	
-	public int result(){
-		return result;
+	public int report(){
+		return final_result.get();
 	}
 	
-	abstract public int method();
+	abstract public int calculate();
 }
 
 /**1.1*/
 class T_1 extends Task{
-	private Integer n;
+	private AtomicInteger n;
 	
 	public T_1(int n){
 		super();
-		this.n = n;
+		this.n = new AtomicInteger(n);
+	}
+	
+	public void set_n(int s){
+		n.addAndGet(-s);
+	}
+	
+	public int get_n(){
+		return n.get();
 	}
 
-	public int method() {
-		synchronized(n){
-			//n -= 
-		}
-		
+	@Override
+	public int calculate() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
@@ -52,7 +53,7 @@ class T_2 extends Task{
 		this.l = l;
 	}
 
-	public int method() {
+	public int calculate() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
