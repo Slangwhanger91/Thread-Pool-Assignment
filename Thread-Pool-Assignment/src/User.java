d3ac9fedb4f392219c29e8576c480441edfc94d3
amit <amitzviklerr@hotmail.com>
2015-04-08 20:19:46 +0300

    Testing mul2

--- Thread-Pool-Assignment/src/User.java
+++ Thread-Pool-Assignment/src/User.java
@@ -6,7 +6,7 @@ import java.util.concurrent.Executors;
 
 
 public class User {
-	
+
 	/**
 	 * @param k - the number of expressions of the form (1.1) that have to be evaluated. <b>(Feeder related)</b>.
 	 * @param r - the number of expressions of the form (1.2) that have to be evaluated. <b>(Feeder related)</b>.
@@ -39,32 +39,68 @@ public class User {
 	}
 
 	public static void main(String[] args) {
-		
-		
+
+
 		int[] n_values_1 = {};//{88,14,365,39,700,17,585,61,94,6};
-		int[] l_values_2 = {5,8,15};//{61,333,24,16,682,11,24,5,476,71};
-		int[] m_values_2 = {9,20,30};//{17,454,8,565,20,27,6,15,657,30};
+		int[] l_values_2 = {5};//{5,8,15};//{61,333,24,16,682,11,24,5,476,71}; mul
+		int[] m_values_2 = {9};//{9,20,30};//{17,454,8,565,20,27,6,15,657,30}; sum
 		int k = n_values_1.length; int r = l_values_2.length;
-		
+
 		int t = 4;
 		int s = 5; int m = 6;
-		
+
 		solution(k, r, n_values_1, l_values_2, m_values_2, t, s, m);
-		
+
 		/*for (int i = 0; i < 10; i++) {
 			System.out.print((int)(Math.random()*100)+",");
 		}*/
-		
+
 		//T_2 t2 = new T_2(m, s);
 		//solution(k, 0, n_values_1, null, null, t, s, m);
 		
-		
+		calcSum1(5, 9);
 
 		
-		
-	}
 }
-	
+
+public static void calcMul1(int mul){
+	double ans = 1;
+	for (int i = 1; i <= mul; i++) {
+		if(i%2 == 0) ans *= (1 / (2.0 * i + 1));
+		else ans *= ((-1) / (2.0 * i + 1));
+	}
+
+	System.out.println("Mul1: " + ans);
+}
+
+public static double calcMul2(int mul){
+	double temp_mul = 1;
+	for (int i = 1; i <= mul; i++) {
+		double temp = (1 / (2.0 * i + 3));
+		if(i % 2 == 0) temp_mul = temp_mul * temp;
+		else temp_mul = temp_mul * (-1) * temp;
+
+	}
+	//System.out.println("Mul2: " + temp_mul);
+	return temp_mul;
+}
+
+public static void calcSum1(int mul, int sum){
+	double temp_sum = 0;
+	int i = 1;
+	for(;i <= sum; i++){
+		double temp = (i / ((2.0 * i * i) + 1));
+	//	System.out.println(i+" : "+temp);
+		temp_sum += temp;
+	}
+
+	System.out.println("mul2: " + (temp_sum + calcMul2(mul)));
+	//System.out.println("sum + mul: " + (temp_sum + calcMul2(mul)));
+//	System.out.println("sum + mul: " + (0==(temp_sum + calcMul2(mul))));
+}
+
+
+}
 
 
 
@@ -94,15 +130,14 @@ public class User {
 
 
 
-
-		/*
+/*
 		PoolManager.PoolThread t1 = new PoolManager(10).new PoolThread();
 		t1.start();
 		try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
 		System.out.println("get state: " + t1.getState());
 		if(t1.getState() == Thread.State.WAITING)
 			System.out.println("Waiting: true");
-		
+
 		int m_perThread = 40;
 		/*
 		T_1 t1 = new T_1(100);
@@ -110,7 +145,7 @@ public class User {
 		MyT mt2 = new MyT(t1, m_perThread);
 		MyT mt3 = new MyT(t1, m_perThread);
 		MyT mt4 = new MyT(t1, m_perThread);
-		
+
 		if(!t1.isDone()){
 			t1.decrease_operation_count(m_perThread, 0);
 			mt1.start();
@@ -127,7 +162,7 @@ public class User {
 			t1.decrease_operation_count(m_perThread, 0);
 			mt4.start();
 		}
-		
+
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
@@ -135,16 +170,16 @@ public class User {
 			e.printStackTrace();
 		}
 		System.out.println("t1 results: " + t1.report());
-		
+
 		//calcMul1(1000);
-		
+
 		 m_perThread = 31;
 		int s_perThread = 26;
 		int MUL = 10;
 		int SUM = 10;
 		//calcMul2(MUL);
 		calcSum1(MUL, SUM);
-		
+
 		T_2 t2 = new T_2(MUL, SUM);
 		MyT2 mt2_1 = new MyT2(t2, m_perThread, s_perThread);
 		MyT2 mt2_2 = new MyT2(t2, m_perThread, s_perThread);
@@ -173,50 +208,15 @@ public class User {
 			e.printStackTrace();
 		}
 		System.out.println("T_2 : "+t2.report());
-		
+
 		//calcMul1(N);
 		//calcMul2(N);
-		
-		
+
+
 	}
 
-	public static void calcMul1(int mul){
-		double ans = 1;
-		for (int i = 1; i <= mul; i++) {
-			if(i%2 == 0) ans *= (1 / (2.0 * i + 1));
-			else ans *= ((-1) / (2.0 * i + 1));
-		}
-		
-		System.out.println("Mul1: " + ans);
-	}
 	
-	public static double calcMul2(int mul){
-		double temp_mul = 1;
-		for (int i = 1; i <= mul; i++) {
-			double temp = (1 / (2.0 * i + 3));
-			if(i % 2 == 0) temp_mul = temp_mul * temp;
-			else temp_mul = temp_mul * (-1) * temp;
 
-		}
-		System.out.println("Mul2: " + temp_mul);
-		return temp_mul;
-	}
-	
-	public static void calcSum1(int mul, int sum){
-		double temp_sum = 0;
-		int i = 1;
-		for(;i <= sum; i++){
-			double temp = (i / ((2.0 * i * i) + 1));
-		//	System.out.println(i+" : "+temp);
-			temp_sum += temp;
-		}
-		
-		System.out.println("sum1: " + temp_sum);
-		//System.out.println("sum + mul: " + (temp_sum + calcMul2(mul)));
-	//	System.out.println("sum + mul: " + (0==(temp_sum + calcMul2(mul))));
-	}
-	
-	
 }
 
 class MyT2 extends Thread{
